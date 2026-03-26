cf053aa61fba3d968122e75be771062b462e8d62
rameesvk551 <rameesvk551@gmail.com>
2026-03-26 19:59:16 +0530

    feat: Add initial partner setup scripts and EC2 environment configuration.

---
+++ server/init_partner.js
@@ -0,0 +1,16 @@
+db.partners.insertOne({
+  _id: ObjectId("69c29fe5c6231cfeec0f14e0"),
+  name: "EdgesPlus",
+  email: "admin@edgesplus.com",
+  apiKeyHash: "$2a$12$7X3PRoL7HmsNpbSG4q8J0ObFp6NjM7gmvrOMXsU3tDhzc7PtH6qfa",
+  apiKeyPrefix: "fb54691a07f9",
+  status: "active",
+  commissionRate: 0.5,
+  walletBalance: 0,
+  totalCustomers: 0,
+  totalRevenue: 0,
+  referralCode: "EDGES1234",
+  plan: "enterprise",
+  createdAt: new Date(),
+  updatedAt: new Date()
+});
---
+++ server/update_partner.js
@@ -0,0 +1,4 @@
+db.partners.updateOne(
+  {_id: ObjectId("69c29fe5c6231cfeec0f14e0")},
+  {$set: {apiKeyHash: "$2b$12$SwsAFZ/dV7mqeqi9YlQQnOZLrQ.Vn75t1T0y41WCBfwBJ0ZzYD0pi"}}
+);
